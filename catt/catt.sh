7212326917f1fdbfd7af9aca85a1d02346b4d34d
dmanojbaba <dmanojbaba@gmail.com>
2026-04-27 01:06:44 +0100

    update catt.sh

--- catt/catt.sh
+++ catt/catt.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-CATT_CFG="--volume /home/pi/dotfiles/catt/catt.cfg:/root/.config/catt/catt.cfg"
-
 if echo "$2" | grep -Eq '^http'; then
-    /usr/bin/docker run --name catt --net=host --rm $CATT_CFG catt:latest /bin/bash -c "catt -d $1 cast $2"
+    /usr/bin/docker exec catt /bin/bash -c "catt -d $1 cast $2"
 else
-    /usr/bin/docker run --name catt --net=host --rm $CATT_CFG catt:latest /bin/bash -c "catt $*"
+    /usr/bin/docker exec catt /bin/bash -c "catt $*"
 fi
